c3a8ee4319e8d01c819c8e57eefb2a2ec4ff8148
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-30 19:26:28 +0300

    init progect

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
